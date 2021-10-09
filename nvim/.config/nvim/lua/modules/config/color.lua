3f15fd48074b03547b103b9f21cc5223df416b94
Harry Tran <harry@coalitioninc.com>
2021-10-09 19:36:07 -0400

    Add missing nvim config file

---
+++ nvim/.config/nvim/lua/modules/config/color.lua
@@ -0,0 +1,29 @@
+return function()
+    require('doom-one').setup({
+            cursor_coloring = true,
+            terminal_colors = false,
+            italic_comments = false,
+            enable_treesitter = true,
+            transparent_background = false,
+            pumblend = {
+                enable = true,
+                transparency_amount = 20,
+            },
+            plugins_integrations = {
+                neorg = true,
+                barbar = false,
+                bufferline = true,
+                gitgutter = false,
+                gitsigns = true,
+                telescope = true,
+                neogit = true,
+                nvim_tree = true,
+                dashboard = false,
+                startify = false,
+                whichkey = true,
+                indent_blankline = true,
+                vim_illuminate = true,
+                lspsaga = false,
+            },
+        })
+end
