11ca28308a6652508714d774ba2817e7b3ba6cfa
Harry Tran <huydtran19@gmail.com>
2023-03-19 14:05:09 -0400

    nvim: close `git` filetype with q

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -37,6 +37,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		"startuptime",
 		"tsplayground",
 		"fugitiveblame",
+		"git",
 	},
 	callback = function(event)
 		vim.bo[event.buf].buflisted = false
