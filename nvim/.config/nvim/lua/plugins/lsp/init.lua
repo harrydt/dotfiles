d74e4140b5aec4bd1539e90a2bf3b4c1e97b65d4
Harry Tran <huydtran19@gmail.com>
2023-07-12 07:29:13 -0700

    issue with capabilities and nvim-cmp-lsp?

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -25,6 +25,8 @@ return {
 				virtual_text = { spacing = 4, prefix = "●" },
 				severity_sort = true,
 			},
+			-- add any global capabilities here
+			capabilities = {},
 			-- Automatically format on save
 			autoformat = true,
 			-- options for vim.lsp.buf.format
@@ -89,8 +91,16 @@ return {
 			vim.diagnostic.config(opts.diagnostics)
 
 			local servers = opts.servers
-			local capabilities =
-				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			-- local capabilities =
+			-- require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				-- require("cmp_nvim_lsp").default_capabilities(),
+				opts.capabilities or {}
+			)
 
 			local function setup(server)
 				local server_opts = vim.tbl_deep_extend("force", {
