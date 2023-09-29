23082518a72cbceaf39131d48de303ddff1d8d6f
Harry Tran <huydtran19@gmail.com>
2023-09-29 15:46:32 -0400

    nvim: add trouble.nvim (back?) and enable telescope for neogit

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -14,6 +14,7 @@ return {
 			},
 			integrations = {
 				diffview = true,
+				telescope = true,
 			},
 		})
 	end,
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -166,4 +166,12 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 	},
+
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
+		},
+	},
 }
