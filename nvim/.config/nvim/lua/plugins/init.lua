896d895113b1b5aa1cd21afdd5f3eb3e66b4fc55
Harry Tran <harry.tran@konghq.com>
2024-05-17 10:19:39 -0400

    nvim: remove commenting plugin
    0.10 has commenting built in

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -10,7 +10,6 @@ return {
 	{ "tpope/vim-surround" },
 	{ "norcalli/nvim-colorizer.lua" },
 	{ "windwp/nvim-autopairs", config = true }, -- See `config` under https://github.com/folke/lazy.nvim#-plugin-spec
-	{ "numToStr/Comment.nvim", config = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{ "kevinhwang91/nvim-bqf" },
 	{ "simrat39/rust-tools.nvim" },
