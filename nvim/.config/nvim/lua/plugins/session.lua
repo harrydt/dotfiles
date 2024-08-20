1f01544ab8cde0cae4ea6f8641190ee1c10d6439
Harry Tran <harry.tran@konghq.com>
2024-08-20 09:26:31 -0400

    nvim: no session for gitcommit or diff

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -6,6 +6,17 @@ return {
 			autoload = true, -- automatically load the session for the cwd on Neovim startup
 			allowed_dirs = { "~/Git", "~/dotfiles" },
 			ignored_dirs = { "~/Dropbox/neorg" },
+			---@return boolean
+			should_save = function()
+				if vim.bo.filetype == "gitcommit" then
+					return false
+				end
+				if vim.opt.diff:get() then
+					return false
+				end
+
+				return true
+			end,
 		})
 	end,
 }
