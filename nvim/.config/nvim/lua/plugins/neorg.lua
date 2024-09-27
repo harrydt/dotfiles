edc751c498942579ba334d9e212c6fecc3bed242
Harry Tran <huydtran19@gmail.com>
2024-09-27 17:42:14 -0400

    nvim: neorg icon

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -18,7 +18,7 @@ local M = {
 					["core.defaults"] = {},
 					["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
 					["core.integrations.nvim-cmp"] = {},
-					["core.concealer"] = { config = { icon_preset = "diamond" } },
+					["core.concealer"] = { config = { icon_preset = "basic" } },
 					["core.export"] = {},
 					["core.keybinds"] = {
 						-- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
