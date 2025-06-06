722422fe9c6d3ed936ace6c688a72ead46408e1e
Harry Tran <huydtran19@gmail.com>
2025-06-06 14:52:57 -0400

    nvim: add neotest-rust

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -6,12 +6,14 @@ local M = {
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
 		{ "fredrikaverpil/neotest-golang", version = "*" },
+		{ "rouge8/neotest-rust" },
 	},
 	config = function()
 		local neotest_golang_opts = {}
 		require("neotest").setup({
 			adapters = {
 				require("neotest-golang")(neotest_golang_opts),
+				require("neotest-rust"),
 			},
 		})
 	end,
