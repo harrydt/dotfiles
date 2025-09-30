b3f4232acaa4ed6564d4a91ada6626e97ed927a5
Harry Tran <huydtran19@gmail.com>
2025-09-29 23:44:43 -0400

    move rust out

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -27,11 +27,6 @@ return {
 		},
 	},
 	{
-		"mrcjkb/rustaceanvim",
-		version = "^6", -- Recommended
-		lazy = false, -- This plugin is already lazy
-	},
-	{
 		"zeioth/garbage-day.nvim",
 		dependencies = "neovim/nvim-lspconfig",
 		event = "VeryLazy",
