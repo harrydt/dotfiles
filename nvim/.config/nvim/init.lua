7d7647341d777817fc960f03861a84febb17a353
Harry Tran <harry.tran@konghq.com>
2025-10-15 16:52:59 -0400

    nvim: load core after plugins to avoid overwrite

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,11 +14,11 @@
 -- Bootstrap Lazy
 require("core/lazy_bootstrap") -- bootstraps folke/lazy
 
+-- Load plugins
+require("lazy").setup("plugins") -- loads each lua/plugins/*
+
 -- Load core settings
 require("core") -- loads lua/core/init.lua
 require("core/options") -- loads lua/core/options.lua
 require("core/autocmds") -- loads lua/core/autocmds.lua
 require("core/keymaps") -- etc.
-
--- Load plugins
-require("lazy").setup("plugins") -- loads each lua/plugins/*
