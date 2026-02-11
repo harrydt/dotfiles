83a0e63c0e2b8be877c49ce80f478bca93446ee3
Harry Tran <harry.tran@konghq.com>
2026-02-11 14:28:49 -0500

    nvim: llm and neotest

--- nvim/.config/nvim/lua/plugins/llm.lua
+++ nvim/.config/nvim/lua/plugins/llm.lua
@@ -1,59 +1,61 @@
 local M = {
-	"olimorris/codecompanion.nvim",
-	version = "v17.33.0", -- avoid breaking changes in v18
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-telescope/telescope.nvim", -- Optional
-		{
-			"stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
-			opts = {},
-		},
-		{
-			"zbirenbaum/copilot.lua",
-			dependencies = {
-				"copilotlsp-nvim/copilot-lsp",
+	{
+		"olimorris/codecompanion.nvim",
+		version = "v17.33.0", -- avoid breaking changes in v18
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- Optional
+			{
+				"stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+				opts = {},
 			},
-			cmd = "Copilot",
-			event = "InsertEnter",
-			config = function()
-				require("copilot").setup({
-					suggestion = { enabled = false },
-					panel = { enabled = false },
-					filetypes = {
-						markdown = true,
-						help = true,
-					},
-				})
-			end,
+			{
+				"zbirenbaum/copilot.lua",
+				dependencies = {
+					"copilotlsp-nvim/copilot-lsp",
+				},
+				cmd = "Copilot",
+				event = "InsertEnter",
+				config = function()
+					require("copilot").setup({
+						suggestion = { enabled = false },
+						panel = { enabled = false },
+						filetypes = {
+							markdown = true,
+							help = true,
+						},
+					})
+				end,
+			},
 		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "copilot",
+					},
+					inline = {
+						adapter = "copilot",
+						keymaps = {
+							accept_change = {
+								modes = { n = "ga" },
+								description = "Accept the suggested change",
+							},
+							reject_change = {
+								modes = { n = "gr" },
+								description = "Reject the suggested change",
+							},
+						},
+					},
+					agent = {
+						adapter = "copilot",
+					},
+				},
+				ignore_warnings = true, -- TODO remove once upgrade to v18
+			})
+		end,
 	},
-	config = function()
-		require("codecompanion").setup({
-			strategies = {
-				chat = {
-					adapter = "copilot",
-				},
-				inline = {
-					adapter = "copilot",
-					keymaps = {
-						accept_change = {
-							modes = { n = "ga" },
-							description = "Accept the suggested change",
-						},
-						reject_change = {
-							modes = { n = "gr" },
-							description = "Reject the suggested change",
-						},
-					},
-				},
-				agent = {
-					adapter = "copilot",
-				},
-			},
-			ignore_warnings = true, -- TODO remove once upgrade to v18
-		})
-	end,
 }
 
 return M
--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -5,15 +5,24 @@ local M = {
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
-		{ "fredrikaverpil/neotest-golang", version = "*" },
+		{
+			"fredrikaverpil/neotest-golang",
+			version = "*", -- Optional, but recommended; track releases
+			build = function()
+				vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+			end,
+		},
 		{ "rouge8/neotest-rust" },
 	},
 	config = function()
-		local neotest_golang_opts = {}
+		local config = {
+			runner = "gotestsum", -- Optional, but recommended
+			-- go_test_args = "-vet=off warn_test_name_dupes = false", -- Too many duplicate test warnings
+			warn_test_name_dupes = false,
+		}
 		require("neotest").setup({
 			adapters = {
-				require("neotest-golang")(neotest_golang_opts),
-				require("neotest-rust"),
+				require("neotest-golang")(config),
 			},
 		})
 	end,
