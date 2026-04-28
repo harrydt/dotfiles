3ab4ccac8116a625d5230a01c6df70b761b8c100
Harry Tran <harry.tran@konghq.com>
2026-04-28 15:25:58 -0400

    feat(nvim): back to bottom search

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -114,7 +114,7 @@ return {
 				},
 			},
 			presets = {
-				bottom_search = false,
+				bottom_search = true,
 				command_palette = true,
 				long_message_to_split = true,
 				lsp_doc_border = true, -- add a border to hover docs and signature help
